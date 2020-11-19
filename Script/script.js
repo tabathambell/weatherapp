b9c2e9d42615cbb5ebbdfd99ff62d680822d29a2
Tabatha Bell <tabathambell@live.com>
2020-11-19 18:56:57 -0500

    finished jquery code and added comments

--- Script/script.js
+++ Script/script.js
@@ -1,19 +1,77 @@
-// beginning of weather app API
-$(document).ready(function() {
-    var url = "api.openweathermap.org/data/2.5/weather?q=orlando,fl,us&appid=a93f43f013a881f05931ff7ec913d0ba"
+/*
 
+DONE: Current weather data.
+DONE: Get UV data (new API call).
+TODO: Get forecast data (new API call).
+TODO: Organize HTML elements.
+TODO: City search
+TODO: Search history
+TODO: UV colors.
+
+*/
+
+$(document).ready(function() {
+    // TODO: Add a search bar in the HTML and use it for the q= parameters in the API endpoint URLs for the city name.
+    // TODO: Wrap all this in a button click event.
+    // TODO: On button click, add to search history.
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=edmonton&appid=737d27db0b50c8b167d4a3cda67efcfe";
+    var foreUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=edmonton&cnt=5&appid=737d27db0b50c8b167d4a3cda67efcfe";
+    
+    // Gets the current weather data.
     $.get(url, function(data) {
-        //variables for icons, different weather patterns
         var city = data.name;
         var icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-        var temp = data.main.temp;
-        var feelsLike = data.main.feels_like;
+        var temp = plzNotKelvin(data.main.temp);
+        var feelsLike = plzNotKelvin(data.main.feels_like);
         var humidity = data.main.humidity;
-        var windSpeed = data.wind.speed;
-        var uv = // OH GOD I DON'T KNOW HOW TO DO THIS ONE
+        var windSpeed = mphWind(data.wind.speed);
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
 
-        alert(JSON.stringify(data, null, 2));
-        $(".actual").text(data.weather[0].description);
+        var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=737d27db0b50c8b167d4a3cda67efcfe";
+
+        // Gets the UV Index.
+        $.get(uvUrl, function(data) {
+            var uvi = data.value;
+            $(".actual").text(uvi);
+            if (uvi < 3) {
+                $(".actual").css("color","green");
+            } else if (uvi < 6) {
+                $(".actual").css("color","yellow");
+            } else if (uvi < 8) {
+                $(".actual").css("color","orange");
+            } else if (uvi < 11) {
+                $(".actual").css("color","red");
+            } else {
+                $(".actual").css("color","purple");
+            }
+        });
+
+        // TODO: Place the weather information into appropriate HTML elements.
         $("body").append('<img src="' + icon + '" alt="' + data.weather[0].description + '" />')
     });
+
+    // Gets the forecast weather data.
+    $.get(foreUrl, function(data) {
+        for (var i = 0; i < data.list.length; i++) {
+            var icon = "http://openweathermap.org/img/wn/" + data.list[i].weather.icon + "@2x.png";
+            var date = moment.unix(data.list[i].dt).format("MMMM Do, YYYY");
+            var dayTemp = plzNotKelvin(data.list[i].temp.day);
+            var nightTemp = plzNotKelvin(data.list[i].temp.night);
+            var humidity = data.list[i].humidity;
+            var windSpeed = mphWind(data.list[i].speed);
+
+            // TODO: Place the weather information into appropriate HTML elements.
+        }
+    });
 });
+
+// Function to convert degrees Kelvin into degrees Fahrenheit.
+function plzNotKelvin(degreesK) {
+    return (degreesK - 273.15) * 9/5 + 32;
+}
+
+// Function to convert wind speed from meters per second into miles per hour.
+function mphWind(windSpeedMS) {
+    return windSpeedMS * 2.237;
+}
