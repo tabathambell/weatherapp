ae3baf75efbc85ce83b44d1181d1de8e5e32c7c3
Tabatha Bell <tabathambell@live.com>
2020-11-19 22:09:51 -0500

    added stylized grids and added more to jquery to place info in grid

--- Script/script.js
+++ Script/script.js
@@ -1,68 +1,67 @@
-
-
 $(document).ready(function() {
 
     $("#weatherButton").click(function() {
-        var url = "http://api.openweathermap.org/data/2.5/weather?q=edmonton&appid=737d27db0b50c8b167d4a3cda67efcfe";
-        var foreUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=edmonton&cnt=5&appid=737d27db0b50c8b167d4a3cda67efcfe";
-    
-    // Gets the current weather data.
-    $.get(url, function(data) {
-        var city = data.name;
-        var icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-        var temp = plzNotKelvin(data.main.temp);
-        var feelsLike = plzNotKelvin(data.main.feels_like);
-        var humidity = data.main.humidity;
-        var windSpeed = mphWind(data.wind.speed);
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
+            var cityName = $("input.form control").val();
 
-        var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=737d27db0b50c8b167d4a3cda67efcfe";
+            var url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=737d27db0b50c8b167d4a3cda67efcfe";
+            var foreUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&cnt=5&appid=737d27db0b50c8b167d4a3cda67efcfe";
+        
+        // Gets the current weather data.
+        $.get(url, function(data) {
+            var city = data.name;
+            var icon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+            var temp = plzNotKelvin(data.main.temp);
+            var feelsLike = plzNotKelvin(data.main.feels_like);
+            var humidity = data.main.humidity;
+            var windSpeed = mphWind(data.wind.speed);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
 
-        // Gets the UV Index.
-        $.get(uvUrl, function(data) {
-            var uvi = data.value;
-            $(".actual").text(uvi);
-            if (uvi < 3) {
-                $(".actual").css("color","green");
-            } else if (uvi < 6) {
-                $(".actual").css("color","yellow");
-            } else if (uvi < 8) {
-                $(".actual").css("color","orange");
-            } else if (uvi < 11) {
-                $(".actual").css("color","red");
-            } else {
-                $(".actual").css("color","purple");
-            }
+            var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=737d27db0b50c8b167d4a3cda67efcfe";
+
+            // Gets the UV Index.
+            $.get(uvUrl, function(data) {
+                var uvi = data.value;
+                $(".actual").text(uvi);
+                if (uvi < 3) {
+                    $(".actual").css("color","green");
+                } else if (uvi < 6) {
+                    $(".actual").css("color","yellow");
+                } else if (uvi < 8) {
+                    $(".actual").css("color","orange");
+                } else if (uvi < 11) {
+                    $(".actual").css("color","red");
+                } else {
+                    $(".actual").css("color","purple");
+                }
+            });
+
+            // Place the weather information into appropriate HTML elements.
+            $("img.current").attr("src", icon);
+            $("h1.current").text(temp);
+            $("p.current").html("Feels Like: " + feelsLike + "<br />");
         });
 
-        // TODO: Place the weather information into appropriate HTML elements.
-        $("body").append('<img src="' + icon + '" alt="' + data.weather[0].description + '" />')
-    });
-
-    // Gets the forecast weather data.
-    $.get(foreUrl, function(data) {
-        for (var i = 0; i < data.list.length; i++) {
-            var icon = "http://openweathermap.org/img/wn/" + data.list[i].weather.icon + "@2x.png";
-            var date = moment.unix(data.list[i].dt).format("MMMM Do, YYYY");
-            var dayTemp = plzNotKelvin(data.list[i].temp.day);
-            var nightTemp = plzNotKelvin(data.list[i].temp.night);
-            var humidity = data.list[i].humidity;
-            var windSpeed = mphWind(data.list[i].speed);
-
-            // TODO: Place the weather information into appropriate HTML elements.
-        }
+        // Gets the forecast weather data.
+        $.get(foreUrl, function(data) {
+            for (var i = 0; i < data.list.length; i++) {
+                var icon = "http://openweathermap.org/img/wn/" + data.list[i].weather.icon + "@2x.png";
+                var date = moment.unix(data.list[i].dt).format("MMMM Do, YYYY");
+                var dayTemp = plzNotKelvin(data.list[i].temp.day);
+                var nightTemp = plzNotKelvin(data.list[i].temp.night);
+                var humidity = data.list[i].humidity;
+                var windSpeed = mphWind(data.list[i].speed);
+            }
+        });
     });
 });
 
 // Function to convert degrees Kelvin into degrees Fahrenheit.
 function plzNotKelvin(degreesK) {
-    return (degreesK - 273.15) * 9/5 + 32;
+    return Math.round((degreesK - 273.15) * 9/5 + 32);
 }
 
 // Function to convert wind speed from meters per second into miles per hour.
 function mphWind(windSpeedMS) {
-    return windSpeedMS * 2.237;
+    return Math.round(windSpeedMS * 2.237);
 }
-
-});
